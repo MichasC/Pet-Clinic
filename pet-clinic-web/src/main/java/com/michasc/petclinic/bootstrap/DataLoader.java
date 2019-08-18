ce567e504d57d50e59bcac1ebf7dd4bad9f733a7
MichasC <m.scibek@outlook.com>
2019-08-18 13:06:04 +0200

    added pets and Contact info t owners on startup. closes #33

--- pet-clinic-web/src/main/java/com/michasc/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/michasc/petclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.michasc.petclinic.bootstrap;
 
 import com.michasc.petclinic.model.Owner;
+import com.michasc.petclinic.model.Pet;
 import com.michasc.petclinic.model.PetType;
 import com.michasc.petclinic.model.Vet;
 import com.michasc.petclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import com.michasc.petclinic.services.PetTypeService;
 import com.michasc.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -38,11 +41,32 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("michael");
         owner1.setLastName("Weston");
+        owner1.setAddress("123 Brickerel");
+        owner1.setCity("Miami");
+        owner1.setTelephone("123123213");
+
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(savedDogPetType);
+        mikesPet.setOwner(owner1);
+        mikesPet.setBirthDate(LocalDate.now());
+        mikesPet.setName("Rosco");
+        owner1.getPets().add(mikesPet);
+
 
         ownerService.save(owner1);
         Owner owner2 = new Owner();
         owner2.setFirstName("Piterson");
         owner2.setLastName("Glamor");
+        owner2.setAddress("123 Brickerel");
+        owner2.setCity("Miami");
+        owner2.setTelephone("123123213");
+
+        Pet fionasCat = new Pet();
+        fionasCat.setName("just Cat");
+        fionasCat.setOwner(owner2);
+        fionasCat.setBirthDate(LocalDate.now());
+        fionasCat.setPetType(savedCatPetType);
+        owner2.getPets().add(fionasCat);
 
         ownerService.save(owner2);
 
